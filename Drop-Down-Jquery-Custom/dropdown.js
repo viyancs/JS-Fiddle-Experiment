f95c7e4ffd3d3e3e4c2239f1f25d7205323615d3
viyancs <msofyancs@gmail.com>
2012-10-15 12:22:29 +0700

    Update Drop-Down-Jquery-Custom/dropdown.js
    adding plugins

--- Drop-Down-Jquery-Custom/dropdown.js
+++ Drop-Down-Jquery-Custom/dropdown.js
@@ -1,16 +1,62 @@
-var result = $("#result");
+(function( $ ){
+    
+    var vobj = {};
+    var res = $("<div id='result' class='result'></div>");
+    
+    /**
+     * initializing 
+     */
+    vobj.init = function(el) {
+        
+        res.css('display','block');
+        res.css('position','relative');
+        res.css('left',el.css('left'));
+        res.css('top',el.css('top'));  
+        res.css('z-index',1000); 
+        res.css('width',el.css('width')); 
+        res.css('height',el.css('height'));  
+        
+        res.appendTo('body');
+    }
+    
+    /**
+     * hide dropdown
+     */        
+     vobj.hide = function(el) {
+        res.fadeOut(1500);
+     }
+
+  $.fn.vdropdown = function(method) {
+     
+    /**
+     * handling statement to execute vobj function  from parameter method
+     */       
+     if(vobj[method]) {
+         return eval('vobj.' + method + '(this)');   
+     }
+ 
+    /**
+     * if method type is object or null return to init function
+     */
+     else if(typeof method === 'object' || !method) {
+         return vobj.init(this);
+     }
+
+    /**         
+     * if method not exist
+     */
+     else {
+         $.error( 'vobj ' +  method + ' does not exist on jQuery.vdropdown' );
+     }
+  };
+
+})( jQuery );
+
 
 $('#dropdown').keydown(function() {
-    result.css('position','relative');
-    result.css('left',$(this).css('left'));
-    result.css('top',$(this).css('top')); 
-    result.css('display','block'); 
-    result.css('z-index',1000); 
-    result.css('width',$(this).css('width')); 
-    result.css('height',$(this).css('height'));    
+    $(this).vdropdown();  
 });
 
 $("#dropdown").blur(function(){
-      result.fadeOut(1500);
+    $(this).vdropdown('hide');
 });
-
