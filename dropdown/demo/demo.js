df9d49490c026875e544d2601a635073993b36d1
viyancs <msofyancs@gmail.com>
2012-10-16 16:07:29 +0700

    Update dropdown/demo/demo.js
    fix if parent don't have child will be skipp create child template

--- dropdown/demo/demo.js
+++ dropdown/demo/demo.js
@@ -1,4 +1,5 @@
 /**
+ * 
  * VDropdown - plugin dropdown using jquery lib
  *
  * generate dropdown using json format
@@ -7,13 +8,15 @@
  * @author M Sofiyan
  * @email msofyancs@gmail.com
  * @skypeid viyancs
+ * @url https://github.com/viyancs/JS-Fiddle-Experiment/tree/master/dropdown
+ * 
  * if you want to using part of full this code, please don't remove this comment
  *
  */
 (function( $ ){
     
     var vobj = {};
-    var res = $("<div class='result'></div>");
+    var res = $("<div class='result-dropdown'></div>");
     var tmpl  = '';
     
     
@@ -24,14 +27,14 @@
         
         //init css
         var css = [{
-        'display':'none',
-        'position':'relative',
-        'left':el.css('left'),
-        'top':el.css('top'),
-        'z-index':1000,
-        'width':el.css('width'),
-        'height':'200px',
-        'overflow':'auto'
+            'display':'none',
+            'position':'absolute',
+            'left':el.offset().left,
+            'top':(el.offset().top + 20),
+            'z-index':1000,
+            'width':el.css('width'),
+            'height':'200px',
+            'overflow':'auto'
         }]; 
         
         res.css(css[0]);
@@ -39,26 +42,27 @@
     }
     
     /**
-     * property url
+     * property 
      */
-     vobj.url = true;
+    vobj.url = true;
+    vobj.id = true;
  
     /**
      * passing from json format
      */
-     vobj.fromJson = function(el){
+    vobj.fromJson = function(el){
         
         //request to server
         $.ajax({
-          url: this.url,
-          async: true,
-          cache: false,
-          method: 'POST',
-          dataType: 'html',//using html becasue i'm not set json header
-          beforeSend: function() {
+            url: this.url,
+            async: true,
+            cache: false,
+            method: 'POST',
+            dataType: 'html',//using html becasue i'm not set json header
+            beforeSend: function() {
                 console.log('Fired prior to the request');
-          },
-          success: function(data) {
+            },
+            success: function(data) {
     
                 var dataJson = JSON.parse(data);
                 var dataTmpl = '';
@@ -83,6 +87,9 @@
                     el.val(val.text());
                     el.attr('data-value',$(this).attr('id'));
                     
+                    //think again
+                    $("#lokasi").val($(this).attr('id'));
+                    
                     e.stopPropagation();
                     
                 });
@@ -92,112 +99,117 @@
                     var val = $(this).find('>:first-child');                    
                     el.val(val.text());
                     el.attr('data-value',val.parent().attr('id'));
+                    $("#lokasi").val($(this).attr('id'));
                     
                     e.stopPropagation(); 
                     
                 });
                     
-          },
-          complete: function() {
+            },
+            complete: function() {
                 console.log('Fired when the request is complete');
-          },
-          error:function(){
-            alert('error');
-           }
+            },
+            error:function(){
+                alert('error');
+            }
         });  
-     }
+    }
     
     /**
      * generate template dropdown 
      */
-     vobj.createTempl = function(valObj) {
+    vobj.createTempl = function(valObj) {
         
         tmpl += '<div id="' + valObj.id +'" class="row-parent" >';
-        tmpl += '<a href="#' + valObj.id +'"><img src="#" width="16px" height="16px">'+ valObj.provinsi +'</a>';
-        $.each(valObj.kota,function(i,value) {
-             tmpl += '<div class="row-child" id="' + value.id + '">'; 
-             tmpl += '<a href="#row-' + value.id + '"><img src="#" width="16px" height="16px">';
-             tmpl += value.name;
-             tmpl += '</a>';
-             tmpl += '</div>';            
-        });
+        tmpl += '<a href="#' + valObj.id +'"><img src="#" width="16px" height="16px">'+ valObj.name +'</a>';
+        
+        if(valObj.kota) {
+            $.each(valObj.kota,function(i,value) {
+                tmpl += '<div class="row-child" id="' + value.id + '">'; 
+                tmpl += '<a href="#row-' + value.id + '"><img src="#" width="16px" height="16px">';
+                tmpl += value.name;
+                tmpl += '</a>';
+                tmpl += '</div>';            
+            });
+            
+        }
         tmpl += '</div>';
         
         return tmpl;
-     }
+    }
          
     /**
      * hide dropdown
      */        
-     vobj.hide = function(el) {
+    vobj.hide = function(el) {
         res.slideUp(1500);
-     }
+    }
      
     /**
      * show dropdown
      */        
-     vobj.show = function(el) {
+    vobj.show = function(el) {
         res.slideDown(1500);
-     }
+    }
 
-  $.fn.vdropdown = function(method) {
+    $.fn.vdropdown = function(method) {
      
-     var el = this;
+        var el = this;
 
-    /**
+        /**
      * handling statement to execute vobj function  from parameter method
      */       
-     if(vobj[method]) {
-         return eval('vobj.' + method + '(this)');   
-     }
+        if(vobj[method]) {
+            return eval('vobj.' + method + '(this)');   
+        }
  
-    /**
+        /**
      * if method type is null return to init function
      */
-     else if(!method) {
-         return vobj.init(this);
-     }
+        else if(!method) {
+            return vobj.init(this);
+        }
          
-    /**
+        /**
      * if method is object , will be passing data from json
      */
-     else if(typeof method === 'object') {
+        else if(typeof method === 'object') {
          
-        $.each(method, function(key, value) {    
+            $.each(method, function(key, value) {    
           
-         //cheking property of object must be defined         
-          if(!vobj[key]) {
+                //cheking property of object must be defined         
+                if(!vobj[key]) {
          
-             $.error( 'vobj ' +  key + ' does not exist on jQuery.vdropdown' );
+                    $.error( 'vobj ' +  key + ' does not exist on jQuery.vdropdown' );
 
-             return false;
+                    return false;
 
-          } else {
+                } else {
              
-             //calling init function
-             vobj.init(el);
+                    //calling init function
+                    vobj.init(el);
 
-             //setting property
-             eval('vobj.' + key + '= value');
+                    //setting property
+                    eval('vobj.' + key + '= value');
              
-             //return fromJson
-             return vobj.fromJson(el);
+                    //return fromJson
+                    return vobj.fromJson(el);
 
-          }         
+                }         
         
-        });
+            });
 
         
-     }
+        }
 
-    /**         
+        /**         
      * if method not exist
      */
-     else {
-         $.error( 'vobj ' +  method + ' does not exist on jQuery.vdropdown' );
-         return false;
-     }
-  };
+        else {
+            $.error( 'vobj ' +  method + ' does not exist on jQuery.vdropdown' );
+            return false;
+        }
+    };
 
 })( jQuery );
 
