1dd64daa2290f857c705daa0f85537c47e47204b
viyancs <msofyancs@gmail.com>
2012-11-19 05:51:57 +0700

    Update rock-paper-scissors/vs-human/demo/demo.js

--- rock-paper-scissors/vs-human/demo/demo.js
+++ rock-paper-scissors/vs-human/demo/demo.js
@@ -1,21 +1,21 @@
-VAR ROCK = 0;
-VAR PAPER = 1;
-VAR SCISSORS = 2;
-VAR LOSE = 'Lose';
-VAR WIN = 'Win';
-VAR DRAW = 'Draw';
-VAR ERR = 'Incorect Value';
+var ROCK = 0;
+var PAPER = 1;
+var SCISSORS = 2;
+var LOSE = 'Lose';
+var WIN = 'Win';
+var DRAW = 'Draw';
+var ERR = 'Incorect Value';
 
 function whoIsWin(a,b) {
 
-	if(a === b) return DRAW;
-	else if(a === ROCK && b === PAPER) return LOSE;
-	else if(a === ROCK && b === SCISSORS) return WIN;
-	else if(a === PAPER && b === ROCK) return WIN;
-	else if(a === PAPER && b === SCISSORS return LOSE;
-	else if(a === SCISSORS && b === ROCK) return LOSE;
-	else if(a === SCISSORS && b === PAPER) return WIN;
-	else return ERR;
+    if(a === b) return DRAW;
+    else if(a === ROCK && b === PAPER) return LOSE;
+    else if(a === ROCK && b === SCISSORS) return WIN;
+    else if(a === PAPER && b === ROCK) return WIN;
+    else if(a === PAPER && b === SCISSORS) return LOSE;
+    else if(a === SCISSORS && b === ROCK) return LOSE;
+    else if(a === SCISSORS && b === PAPER) return WIN;
+    else return ERR;
 }
 
 var who = whoIsWin(ROCK,PAPER);
