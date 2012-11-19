0d608cb1d52f4431034b51f590c3ed4acb0d5671
viyancs <msofyancs@gmail.com>
2012-11-20 03:12:58 +0700

    update demo.js

--- rock-paper-scissors/vs-com/demo/demo.js
+++ rock-paper-scissors/vs-com/demo/demo.js
@@ -1,3 +1,6 @@
+var who = whoIsWin(ROCK,PAPER);
+console.log(who);
+
 var ROCK = 0;
 var PAPER = 1;
 var SCISSORS = 2;
@@ -6,7 +9,12 @@ var WIN = 'Win';
 var DRAW = 'Draw';
 var ERR = 'Incorect Value';
 
-function whoIsWin(a,b) {
+function whoIsWin(a) {
+    
+    var rps = [ROCK,PAPER,SCISSORS];
+    var random = rps.sort(shuffle); //sorting to random
+    var b = random[0];
+    console.log('computer is =' + b);
 
     if(a === b) return DRAW;
     else if(a === ROCK && b === PAPER) return LOSE;
@@ -18,5 +26,9 @@ function whoIsWin(a,b) {
     else return ERR;
 }
 
-var who = whoIsWin(ROCK,PAPER);
+function shuffle(a,b) {
+	return Math.random() > 0.5 ? -1 : 1;
+}
+
+var who = whoIsWin(ROCK);
 console.log(who);
